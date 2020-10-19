a07a6660db19645a3073af0eea1d97f0f0b1c0e4
arnoldruiz115 <arnold.r115@gmail.com>
2020-10-19 14:44:24 -0700

    Added basic movement

--- arcade.py
+++ arcade.py
@@ -1,5 +1,5 @@
 import pygame
-import sys
+import game_functions as gf
 from settings import Settings
 from player import Player
 
@@ -7,22 +7,17 @@ from player import Player
 def run_game():
     pygame.init()
     settings = Settings()
-
     screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
     bg = (200, 170, 255)
 
-    player = Player(settings)
-    sprites = pygame.sprite.Group()
-    sprites.add(player)
+    player = Player(settings, screen)
 
     run = True
     while run:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
         screen.fill(bg)
-        for entity in sprites:
-            screen.blit(entity.surface, entity.rect)
+        gf.check_events(player)
+        player.blitme()
+        player.update()
         pygame.display.update()
 
 
--- player.py
+++ player.py
@@ -2,8 +2,9 @@ import pygame
 
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, settings):
+    def __init__(self, settings, screen):
         super().__init__()
+        self.screen = screen
         self.speed = 5
         self.player_x = settings.screen_width / 2
         self.player_y = settings.screen_height / 2
@@ -11,4 +12,26 @@ class Player(pygame.sprite.Sprite):
         self.player_h = 40
         self.surface = pygame.Surface((self.player_w, self.player_h))
         self.surface.fill((250, 100, 10))
-        self.rect = self.surface.get_rect(center=(self.player_x, self.player_y))
+        self.center = (self.player_x, self.player_y)
+        self.rect = self.surface.get_rect()
+        self.rect.centerx = float(self.player_x)
+        self.rect.bottom = float(self.player_y)
+
+        self.moving_left = False
+        self.moving_right = False
+        self.moving_up = False
+        self.moving_down = False
+
+    def blitme(self):
+        self.screen.blit(self.surface, self.rect)
+
+    def update(self):
+        if self.moving_left:
+            self.rect.centerx -= 1
+        if self.moving_right:
+            self.rect.centerx += 1
+        if self.moving_up:
+            self.rect.bottom -= 1
+        if self.moving_down:
+            self.rect.bottom += 1
+
